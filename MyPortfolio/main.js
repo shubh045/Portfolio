3690eb7be502d2be6d6f73fa464941df56acb0f2
shubh045 <shubh6441@gmail.com>
2022-02-02 19:52:49 +0530

    updated file

--- MyPortfolio/main.js
+++ MyPortfolio/main.js
@@ -1,18 +1,15 @@
-const navItems = document.querySelector('.nav-items');
+// const navItems = document.querySelector('.nav-items');
 const heroNav = document.querySelector('.heronav');
-const brandName = document.querySelector('.brand-name');
+// const brandName = document.querySelector('.brand-name');
+// const navbarToggler = document.querySelector('.button-nav').getAttribute('aria-expanded').valueOf();
+// const toggling = document.querySelector('.toggling');
+//  const show = document.querySelector('.show');
+
 
 window.onscroll = () => {
     let scroll = document.documentElement.scrollTop;
     const headerHeight = document.documentElement.clientHeight * 0.80;
-    const headerBottomHeight = headerHeight - 154;
-
-    if (scroll > headerBottomHeight && scroll < headerHeight) {
-        navItems.classList.add('removeNav');
-    }
-    else {
-        navItems.classList.remove('removeNav');
-    }
+    // const headerBottomHeight = headerHeight + 50;
 
     if (scroll > headerHeight) {
         heroNav.classList.add('newHeroNav');
@@ -20,9 +17,20 @@ window.onscroll = () => {
         heroNav.classList.remove('newHeroNav');
     }
 
-    if (scroll > 100 && scroll < headerHeight) {
-        brandName.classList.add("newBrandName");
+    if (scroll > 80 && scroll < headerHeight) {
+        heroNav.classList.add('removeNav');
+        // brandName.classList.add("newBrandName");
+        // navbarToggler.classList.add("button-nav");
     } else {
-        brandName.classList.remove("newBrandName");
+        heroNav.classList.remove('removeNav');
+        // brandName.classList.remove("newBrandName");
+        // navbarToggler.classList.remove("button-nav");
     }
 }
+
+// navItems.style("padding-left: 0");
+// if(toggling.getAttribute('class').valueOf() == 'show'){
+//     toggling.classList.add('toggleButton')
+// }else{
+//     toggling.classList.remove('toggleButton');
+// }
